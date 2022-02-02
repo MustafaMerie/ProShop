e458bf9a27f4974a24e55493c6deaaf3f56f5485
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-02-02 19:16:08 +0300

    Initialize the fronend UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      Welcome
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>}/>
+            <Route path='/product/:id' element={<ProductScreen/>} />
+            </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
