1054253f87bee51a8fbaaafc95762153289672f4
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-04-28 22:59:42 +0300

    user login screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,13 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReduces } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReduces
 });
 
 
@@ -18,10 +20,15 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+  const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : [];
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
-  }
+  },
+  userLogin: {userInfo: userInfoFromStorage}
 };
 
 const middleware = [thunk];
