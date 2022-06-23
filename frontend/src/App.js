550577605c65968fc0d0ff85f3ddabf7f61ab14a
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-06-23 19:39:10 +0300

    place checkout screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/loginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const App = () => {
   return (
@@ -23,6 +26,9 @@ const App = () => {
             <Route path='/product/:id' element={<ProductScreen/>} />
             <Route path='/cart/' element={<CartScreen/>} />
             <Route path='/cart/:id' element={<CartScreen/>} />
+            <Route path='/shipping' element={<ShippingScreen/>}/>
+            <Route path='/payment' element={<PaymentScreen/>}/>
+            <Route path='/placeorder' element={<PlaceOrderScreen/>}/>
             <Route path='/' element={<HomeScreen/>}/>
             </Routes>
         </Container>
