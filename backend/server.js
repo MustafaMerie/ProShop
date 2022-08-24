32d4c840623a0cd7ca9ae8da4bb5b750025e6112
Mustafa Nawzat <mustafanawzatt@gmail.com>
2022-08-24 15:04:46 +0300

    deploy on Heroku

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-res.send ('API is running....')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -32,6 +29,18 @@ app.get('/api/config/paypal',(req,res)=>
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }  
 
 const PORT = process.env.PORT || 5000
 app. listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
